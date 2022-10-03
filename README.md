ece3469d55c17cba712cdc072c93487aae953622
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-10-04 01:42:28 +0500

    Copy all todo-list files

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# TodoList-Best-Practices
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# To Do List
+
+> _To-do list_ is a tool that helps to organize a day. It lists the things that user need to do and allows to mark them as complete. This website is built using ES6 and Webpack!
+
+## Built With
+
+- HTML
+- CSS
+- JavaScript
+- Webpack
+
+## Live Demo
+
+[Live Demo Link](https://kanzatahreem.github.io/To-Do-List/)
+
+## Getting Started
+
+To get a local copy, run the fllowing command.
+
+- `git clone git@github.com:KanzaTahreem/To-Do-List.git`
+
+### Prerequisites
+
+- node.js (10.13.0 (LTS))
+- git
+
+### Install
+
+- `npm install`
+
+### Usage
+
+`npm start`
+
+## Authors
+
+👤 **Author**
+
+- GitHub: [@KanzaTahreem](https://github.com/KanzaTahreem)
+- Twitter: [@kanza_tahreem](https://twitter.com/kanza_tahreem)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/kanza-tahreem/)
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## 📝 License
+
+This project is [MIT](./LICENSE) licensed.
---
+++ src/index.js
@@ -0,0 +1,40 @@
+/* eslint-disable */
+import _, { add } from 'lodash';
+/* eslint-enable */
+import './style.css';
+import { addListToLocalStorage, loadListFromLocalStorage } from './modules/localStorage.js';
+import { addToDo, toDoLi } from './modules/ui_handlers.js';
+import ToDoList from './modules/todolist.js';
+
+const submitBtn = document.querySelector('.submit-btn');
+const clearAllBtn = document.querySelector('.clear-btn');
+const addToDoInput = document.querySelector('.add-todo');
+
+const toDoList = new ToDoList();
+const dataFromLocalStorage = loadListFromLocalStorage();
+
+dataFromLocalStorage.forEach((toDoObject) => {
+  const pushedLocalTask = toDoList.addNewTask(
+    toDoObject.index, toDoObject.description, toDoObject.isCompleted,
+  );
+  addToDo(pushedLocalTask, toDoList);
+});
+
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const inputValue = addToDoInput.value;
+  const pushedTask = toDoList.addNewTask(null, inputValue, false);
+  addToDoInput.value = '';
+  addToDo(pushedTask, toDoList);
+  addListToLocalStorage(toDoList.list);
+});
+
+clearAllBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  toDoList.removeCompletedTask();
+  toDoLi.innerHTML = '';
+  toDoList.list.forEach((task) => {
+    addToDo(task, toDoList);
+  });
+  addListToLocalStorage(toDoList.list);
+});
